4b2084ed8eaa93b0c05be6c4cb7a04d9ecbb1a19
S.Kobayashi <shunsuke.kobayashi@rakus.co.jp>
2025-05-20 14:04:42 +0900

    ログインフォームの追加

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
 import com.example.form.InsertAdministatorForm;
+import com.example.form.LoginForm;
 import com.example.repository.AdministratorRepository;
 import com.example.service.AdministratorService;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +21,24 @@ public class AdministratorController {
     private AdministratorService administratorService;
 
     /**
-     *admin/insert.html にフォワードする処理を記述する.
+     *administrator/insert.htmlにフォワードする処理を記述する.
      *
-     * @param form　従業員登録する際のリクエストパラメータ
+     * @param form　従業員登録する際の入力フォーム
      * @return フォワード処理
      */
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministatorForm form){
+        return "administrator/insert";
+    }
 
+    /**
+     * "administrator/login.htmlにフォワードする処理を記述する.
+     *
+     * @param form ログインする際の入力フォーム
+     * @return フォワード処理
+     */
+    @GetMapping("/")
+    public String toLogin(LoginForm form){
+        return  "administrator/login";
     }
 }
